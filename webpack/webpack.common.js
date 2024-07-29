30164816243ff9e77e2c7eb50f6aa745223576f7
tngo2001 <61449896+tngo2001@users.noreply.github.com>
2024-07-29 13:38:10 -0700

    Fixed vulnerabilities

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -28,7 +28,7 @@ module.exports = {
     }
   },
   plugins: [
-    new HtmlWebpackPlugin({ gameName: 'My Phaser Game', template: 'src/index.html' }),
+    new HtmlWebpackPlugin({ gameName: 'Camp Code', template: 'src/index.html' }),
     new CopyWebpackPlugin({
       patterns: [
       { from: 'src/assets', to: 'assets' },
